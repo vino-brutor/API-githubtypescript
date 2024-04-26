0638cc91d2ef817ea0b5567dbd95679e3fca2b3a
vino-brutor <viktorbruno2006@gmail.com>
2024-04-26 11:10:23 -0300

    fixes

--- src/index.ts
+++ src/index.ts
@@ -18,61 +18,32 @@ let allUsers:User[] = []
 
 
 
-// async function getUser(){
-//     const user = prompt('Qual usuário do github gostaria de pesquisar')
-//     const response = await fetch(`https://api.github.com/users/${user}`)
-//     const userResponse = await response.json()
+async function getUser(user:string){
+    // const user = prompt('Qual usuário do github gostaria de pesquisar')
+    const response = await fetch(`https://api.github.com/users/${user}`)
+    const userResponse = await response.json()
     
-//     if(userResponse.message || userResponse.login === null){
-//         console.log("User not found")
-//     }else{
-//         console.log(userResponse)
-//         let userFiltered:User = {
-//             id: userResponse.id,
-//             login: userResponse.login,
-//             name: userResponse.name,
-//             bio: userResponse.bio,
-//             publicRepos: userResponse.public_repos,
-//             reposUrl: userResponse.repos_url
-//         }
-//         console.log(userFiltered)
-//         console.log(allUsers)
-//         allUsers.push(userFiltered)
-//     }
-// }
-
-function getUser(): Promise<void> {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const user = prompt('Qual usuário do github gostaria de pesquisar');
-            const response = await fetch(`https://api.github.com/users/${user}`);
-            const userResponse = await response.json();
-            
-            if (userResponse.message || userResponse.login === null) {
-                console.log("User not found");
-            } else {
-                console.log(userResponse);
-                let userFiltered: User = {
-                    id: userResponse.id,
-                    login: userResponse.login,
-                    name: userResponse.name,
-                    bio: userResponse.bio,
-                    publicRepos: userResponse.public_repos,
-                    reposUrl: userResponse.repos_url
-                };
-                console.log(userFiltered);
-                console.log(allUsers);
-                allUsers.push(userFiltered);
-            }
-            resolve();
-        } catch (error) {
-            reject(error);
+    if(userResponse.message || userResponse.login === null){
+        console.log("User not found")
+    }else{
+        console.log(userResponse)
+        let userFiltered:User = {
+            id: userResponse.id,
+            login: userResponse.login,
+            name: userResponse.name,
+            bio: userResponse.bio,
+            publicRepos: userResponse.public_repos,
+            reposUrl: userResponse.repos_url
         }
-    });
+        console.log(userFiltered)
+        console.log(allUsers)
+        allUsers.push(userFiltered)
+    }
 }
 
-async function showUser() {
-    const userName = prompt('De qual usuário gostaria de ver os repositórios?');
+
+async function showUser(userName:string) {
+    // const userName = prompt('De qual usuário gostaria de ver os repositórios?');
     const findUser = allUsers.find((user) => user.login === userName);
     console.log(findUser)
     if (findUser) {
@@ -84,7 +55,7 @@ async function showUser() {
         });
         alert(listOfRepository);
     } else {
-        alert(`Usuário não encontrado na lista`);
+        console.log(`Usuário não encontrado na lista`);
     }
 }
 
@@ -93,12 +64,12 @@ function sumRepos(){
     allUsers.map((user) => {
         totalRepos += user.publicRepos
     })
-    alert(`No total há na lista ${totalRepos} na lista`)
+    console.log(`No total há na lista ${totalRepos} na lista`)
 }
 
 function shouUsers(){
     allUsers.forEach(user => {
-        alert(`
+        console.log(`
         Name: ${user.name}
         Login:${user.login}
         Bio: ${user.bio}
@@ -110,48 +81,7 @@ function shouUsers(){
 function topFive() {
     allUsers.sort((a, b) => b.publicRepos - a.publicRepos);
     for (let index = 0; index < 5; index++) {
-        alert(`Usuários com mais repositórios públicos:
+        console.log(`Usuários com mais repositórios públicos:
         ${index + 1} - Name: ${allUsers[index].name}, Public Repositories: ${allUsers[index].publicRepos}`);
     }
 }
-
-function iniciate(){
-    let option:number
-    do{
-        option = Number(prompt(`Escolha uma das opções abaixo:
-        1 - Cadastrar usuário do github
-        2 - Pesquisar usuário cadastrado
-        3 - Total de repositórios na lista
-        4 - Exibir todos os usuários
-        5 - Top 5 usuários com mais repositórios públicos
-        6 - sair`))
-        switch(option){
-            case 1:
-                getUser().then(() => {
-
-                }).catch((error) => {
-                    console.log('Erro ao cadastrar suário:', error)
-                })
-                break
-            case 2:
-                showUser()
-                break
-            case 3: 
-                sumRepos()
-                break
-            case 4:
-                shouUsers()
-                break
-            case 5:
-                topFive()
-                break
-            case 6:
-                break
-            default:
-                alert('Opção inválida')
-        }
-    }while(option != 6)
-    
-    alert(`Saindo...`)
-    }
-iniciate()
